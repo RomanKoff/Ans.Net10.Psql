595dbbb2c4be74b842afc8598dc15450e3d9afd3
Roman Koff <krv@guap.ru>
2026-01-21 14:25:48 +0300

    2026-001-21

---
+++ LibPsqlInfo.cs
@@ -0,0 +1,13 @@
+﻿using Ans.Net10.Common;
+
+namespace Ans.Net10.Psql
+{
+
+	public static class LibPsqlInfo
+	{
+		public static string GetName() => SuppApp.GetName();
+		public static string GetVersion() => SuppApp.GetVersion();
+		public static string GetDescription() => SuppApp.GetDescription();
+	}
+
+}
